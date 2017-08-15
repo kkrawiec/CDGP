ad717dc1b07d0f185a0b5e146467a5a590a05b97
Iwo Bladek <iwo.bladek@cs.put.poznan.pl>
2017-08-15 19:10:54 +0200

    Move CDGPEvaluation classes to a distinct file

--- src/sygusgp/CDGPState.scala
+++ src/sygusgp/CDGPState.scala
@@ -1,7 +1,5 @@
 package sygusgp
 
-import fuel.core.StatePop
-import fuel.func.{Evaluation, ParallelEval, SequentialEval}
 import fuel.util.{Collector, Options, TRandom}
 import swim.tree.Op
 import sygus.VarDeclCmd
@@ -53,6 +51,11 @@ class TestsManagerCDGP[I,O]() {
 }
 
 
+/**
+  * Manages everything needed for the CDGP to run. Among other, handles interaction with the solver, and
+  * contains test manager. As argument given is the definition of the SYGUS problem to be solved, and from
+  * it extracted are all important information such as grammar and logic to be used.
+  */
 class CDGPState(sygusProblem: SyGuS16)
                (implicit opt: Options, coll: Collector, rng: TRandom) {
   // The types for input and output
@@ -259,11 +262,6 @@ class CDGPState(sygusProblem: SyGuS16)
     }
   }
 
-//  def eval[S]: S => E = searchAlg match {
-//    case "GP" | "GPSteadyState" => evalInt
-//    case "Lexicase" | "LexicaseSteadyState" => evalSeqInt
-//  }
-
   /**
     * Creates CDGPEvaluation based on provided settings in the options.
     */
@@ -289,86 +287,3 @@ object CDGPState {
     new CDGPState(sygusProblem)
 }
 
-
-
-
-/**
-  * CDGPEvaluation performs evaluation in the spirit of Counterexample-Driven Genetic Programming
-  * (see the paper: I.Błądek, K.Krawiec, J.Swan, "Counterexample-Driven Genetic Programming", GECCO'17).
-  *
-  * GP starts with an empty test set and a formal specification of the correct program. In the most
-  * conservative variant only individual passing all test cases is verified for consistency with
-  * the specification by the SMT solver. If such a verification fails, solver returns a counterexample.
-  * This counterexample is then added to the test set and the process continues.
-  *
-  * @param cdgpState Object handling the state of the CDGP.
-  * @param eval Function from a solution to its evaluation. It is also dependent on the cdgpState,
-  *             but because of many possible variants it should be created outside.
-  * @param opt Options.
-  * @param coll Collector for saving results and stats.
-  * @param rng Pseudorandom generator.
-  * @tparam S Type of solution representation object (e.g. String, Seq[Int]).
-  * @tparam E Type of evaluation object (e.g. Int, Seq[Int]).
-  */
-class CDGPEvaluation[S, E](val cdgpState: CDGPState,
-                           val eval: (S) => E)
-                          (implicit opt: Options, coll: Collector, rng: TRandom)
-      extends Evaluation[S, E] {
-  private val silent = opt('silent, false)
-  def evaluate: Evaluation[S, E] = if (opt('parEval, true)) ParallelEval(eval) else SequentialEval(eval)
-
-  override def apply(s: StatePop[S]): StatePop[(S, E)] = cdgpEvaluate(s)
-
-  def cdgpEvaluate: StatePop[S] => StatePop[(S, E)] =
-    evaluate andThen updatePopulationEvalsAndTests
-
-  def updatePopulationEvalsAndTests(s: StatePop[(S, E)]): StatePop[(S, E)] = {
-    // For generational GP there is no need to update evals, because all individuals are being replaced.
-    updateTestsManager()
-    s
-  }
-
-  def updateTestsManager() {
-    val numNew = cdgpState.testsManager.newTests.size
-    cdgpState.testsManager.flushHelpers()  // adds newTests to all tests; resets newTests
-    if (!silent) {
-      val numKnown = cdgpState.testsManager.tests.values.count(_.isDefined)
-      println(f"Tests: found: ${numNew}  total: ${cdgpState.testsManager.tests.size}  known outputs: $numKnown")
-    }
-  }
-}
-
-
-/**
-  * A modified version of CDGPEvaluation which is specilized in accomodating steady state GP.
-  * In steady state evaluated are, beside initial population, only single solutions (selected in the
-  * given iteration). After such evaluation the number of test cases can increase. Thus,
-  * updatePopulationEvalsAndTests must be invoked for example at the end of the iter function.
-  *
-  * @param cdgpState Object handling the state of the CDGP.
-  * @param eval Function from a solution to its evaluation. It is also dependent on the cdgpState,
-  *             but because of many possible variants it should be created outside.
-  * @param updateEval CDGP may add a new test after evaluation of a selected individual.
-  *                   This means that evaluations of all individuals in the population need
-  *                   to be updated. This is handled by this function.
-  * @param opt Options.
-  * @param coll Collector for saving results and stats.
-  * @param rng Pseudorandom generator.
-  * @tparam S Type of solution representation object (e.g. String, Seq[Int]).
-  * @tparam E Type of evaluation object (e.g. Int, Seq[Int]).
-  */
-class CDGPEvaluationSteadyState[S, E](cdgpState: CDGPState,
-                                      eval: S => E,
-                                      updateEval: ((S, E)) => (S, E))
-                                     (implicit opt: Options, coll: Collector, rng: TRandom)
-      extends CDGPEvaluation[S, E](cdgpState, eval) {
-
-  override def updatePopulationEvalsAndTests(s: StatePop[(S, E)]): StatePop[(S, E)] = {
-    val s2 =
-      if (cdgpState.testsManager.newTests.nonEmpty)
-        StatePop(s.map{ case (op, e) => updateEval(op, e) })
-      else s
-    updateTestsManager()
-    s2
-  }
-}
