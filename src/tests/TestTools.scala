a84ec212d5c69befaf78e76e7507cbbb13d01aa3
Iwo Bladek <iwo.bladek@cs.put.poznan.pl>
2017-10-05 11:56:13 +0200

    Parser for properties files

---
+++ src/tests/TestTools.scala
@@ -0,0 +1,28 @@
+package tests
+
+import cdgp.{PropertiesParser, Tools}
+import org.junit.Test
+import org.junit.Assert._
+
+final class TestTools {
+  @Test
+  def testParsePropertiesFileEmpy(): Unit = {
+    val text =
+      """
+        |
+        |
+      """.stripMargin
+    val opt = Tools.parsePropertiesFile(text)
+    assertEquals(Map(), opt.allOptions)
+  }
+
+  @Test
+  def testParsePropertiesFile(): Unit = {
+    val text =
+      """CDGPoneTestPerIter = false
+        |CDGPtestsRatio = 0.5
+      """.stripMargin
+    val opt = Tools.parsePropertiesFile(text)
+    assertEquals(Map("CDGPoneTestPerIter"->"false", "CDGPtestsRatio"->"0.5"), opt.allOptions)
+  }
+}
